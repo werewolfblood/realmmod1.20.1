104d033d105792fba3750d1b100dd462618c0056
werewolfblood <Deep.mines.rpg@gmail.com>
2024-11-12 09:06:58 -0800

    Initial commit

---
+++ src/main/java/dev/west/realmmod/RealmModClient.java
@@ -0,0 +1,10 @@
+package dev.west.realmmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RealmModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
